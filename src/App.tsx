f50ac1e302bfa67f40b9856692d23143c2fafd5e
resourge <jdaoliveiraprograming@hotmail.com>
2023-08-11 10:56:46 +0100

    chore(app): remove test changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,3 @@
-import { SetupPaths, path } from './lib'
-
-const RoutePaths = SetupPaths({
-	HOME: path(),
-	PRODUCT: path('product')
-	.routes({
-		FORM: path()
-		.param('productId')
-	})
-})
-
-console.log('asd', RoutePaths.HOME
-.withSearchParams({
-	id: 10,
-	test: {
-		id: 10
-	}
-})
-.get())
-
 function App() {
 	return (
 		<div>
