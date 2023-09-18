c1405cc5d8993676b547068da145afba510298d7
resourge <jdaoliveiraprograming@hotmail.com>
2023-09-18 14:10:05 +0100

    fix(entries): possible performance improve on generic type entries

--- src/lib/types/Entries.ts
+++ src/lib/types/Entries.ts
@@ -1,10 +1,40 @@
-type UnionToIntersection<U> = (U extends never ? never : (arg: U) => never) extends (arg: infer I) => void
-	? I
-	: never;
+/**
+ * Convert union U to an intersection comprised of each constituent type of U
+ *
+ * e.g. UnionToIntersection<1 | 2 | 3> // 1 & 2 & 3
+ */
+type UnionToIntersection<U> =
+  (U extends unknown ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never
 
-type UnionToTuple<T, A extends any[] = []> = UnionToIntersection<T extends never ? never : (t: T) => T> extends (_: never) => infer W
-	? UnionToTuple<Exclude<T, W>, [...A, W]>
-	: A;
+/**
+ * Return the last constituent type of union T
+ * ex. UnionPop<1 | 2 | 3> // 3
+ */
+type UnionPop<T> = FunctionalIntersection<T> extends ((_: infer U) => void) ? U : never
+type FunctionalIntersection<T> = UnionToIntersection<FunctionalUnion<T>>
+type FunctionalUnion<T> = T extends unknown ? (_: T) => void : never
+
+/**
+ * Return a new tuple with H prepended to T
+ * ex. TupleUnshift<1, [ 2, 3 ]> // [ 1, 2, 3 ]
+ */
+type TupleUnshift<H, T extends unknown[]> =
+  ((head: H, ...tail: T) => void) extends ((..._: infer U) => void) ? U : never
+
+/**
+ * Return a tuple composed of each constituent type of union U
+ * ex. UnionToTuple<1 | 2 | 3> // [ 1, 2, 3 ]
+ *     UnionToTuple<1 | 2 | 3, [ 4 ]> // [ 1, 2, 3, 4 ]
+ *
+ * Since recursion is used to instantiate this type, surprising things happen when passing
+ * larger unions (22+ constituents). TypeScript begins to detect that the result _may_
+ * cause an infinite recursion, and bails out by giving an `any` type.
+ */
+type UnionToTuple<T, R extends unknown[] = []> = {
+	next: UnionPop<T> extends infer Tail
+		? UnionToTuple<Exclude<T, Tail>, TupleUnshift<Tail, R>> : never
+	result: R
+}[[T] extends [never] ? 'result' : 'next']
 
 export type Entries<T> = UnionToTuple<{
 	[K in keyof T]: [K, T[K]]
