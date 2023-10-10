2555f3e1ec5d7dbb139f853168dfe823c6268383
resourge <jdaoliveiraprograming@hotmail.com>
2023-10-10 16:34:53 +0100

    perf(types): improve MergeParamsAndCreate type

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -30,14 +30,31 @@ export type GetValueFromTransform<Config extends ParamsConfig = ParamsConfig> = 
 	? string 
 	: ReturnType<Config['transform'] extends (...args: any[]) => any ? Config['transform'] : (value: any) => string>
 
+type OptionalPropertyNames<T> =
+  { [K in keyof T]-?: ({} extends { [P in K]: T[K] } ? K : never) }[keyof T];
+
+type SpreadProperties<L, R, K extends keyof L & keyof R> =
+  { [P in K]: L[P] | Exclude<R[P], undefined> };
+
+type Id<T> = T extends infer U ? { [K in keyof U]: U[K] } : never
+
+type SpreadTwo<L, R> = Id<
+  & Pick<L, Exclude<keyof L, keyof R>>
+  & Pick<R, Exclude<keyof R, OptionalPropertyNames<R>>>
+  & Pick<R, Exclude<OptionalPropertyNames<R>, keyof L>>
+  & SpreadProperties<L, R, OptionalPropertyNames<R> & keyof L>
+>;
+
+type Spread<L, R = any> = SpreadTwo<L, R>
+
 export type MergeParamsAndCreate<
-	T extends Record<string, any>, 
-	U extends string, 
+	Params extends Record<string, any>, 
+	Key extends string, 
 	IsOptional extends boolean | undefined,
 	V
-> = string extends keyof T
-	? CreateObject<U, V, IsOptional>
-	: T & CreateObject<U, V, IsOptional>
+> = string extends keyof Params
+	? CreateObject<Key, V, IsOptional>
+	: Spread<Params, CreateObject<Key, V, IsOptional>>
 
 export type MergeObj<
 	T extends Record<string, any>, 
