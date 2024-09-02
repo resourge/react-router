f8b38c3091494137150f4b7e81872404199432c2
resourge <jdaoliveiraprograming@hotmail.com>
2024-09-02 17:26:47 +0100

    fix(vitest): fix vitest tests not working with @resourge/history-store

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ command }) => {
 	return {
 		define: {
 			__DEV__: command === 'serve'
@@ -11,7 +12,21 @@ export default defineConfig(({ command, mode }) => {
 		test: {
 			globals: true,
 			environment: 'jsdom',
-			setupFiles: './src/setupTests.ts'
+			setupFiles: './src/setupTests.ts',
+			
+			deps: {
+				optimizer: {
+					web: { 
+						enabled: true,
+						include: [
+							'@resourge/history-store/utils'
+						]
+					}	
+				}
+			}
+		},
+		resolve: {
+			preserveSymlinks: true
 		},
 		plugins: [
 			react(),
